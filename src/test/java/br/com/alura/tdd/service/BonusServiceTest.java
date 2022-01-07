0f2f1480cfbd4787a5c024976e2d4b6ae3b22ede
Joao Victor <joaovsg@id.uff.br>
2022-01-07 16:47:23 -0300

    Implementing the unit test to BonusService class

---
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,38 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10001")));
+
+        Assertions.assertEquals(BigDecimal.ZERO,bonus);
+    }
+
+    @Test
+    void bonusDeveriaSer10PorCentoDoSalario(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
+
+        Assertions.assertEquals(new BigDecimal("250.0"),bonus);
+    }
+
+    @Test
+    void bonusDeveriaSerDezPorCentoParaSalarioDe10000(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
+
+        Assertions.assertEquals(new BigDecimal("1000.0"),bonus);
+    }
+
+}
